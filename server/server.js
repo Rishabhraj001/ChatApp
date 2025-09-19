0b9986859e014af8f40816968a44acc35c871c50
Rishabhraj001 <rishabhraj1130jnvbgp@gmail.com>
2025-09-19 14:56:21 +0530

    ok

--- server/server.js
+++ server/server.js
@@ -11,43 +11,46 @@ const app = express()
 const server = http.createServer(app)
 
 // Initialize socket.io
-export const io = new Server(server,{
-    cors:{origin:"*"}
+export const io = new Server(server, {
+    cors: { origin: "*" }
 })
 
 //store online users
 export const userSocketMap = {}
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
-    console.log("User Connected",userId)
+    console.log("User Connected", userId)
 
-    if(userId) userSocketMap[userId] = socket.id
+    if (userId) userSocketMap[userId] = socket.id
 
     //emit online users to all connected clients
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected",userId);
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
         for (const id in userSocketMap) {
             if (userSocketMap[id] === socket.id) {
                 delete userSocketMap[id];
                 break;
             }
         }
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
     })
 })
 
 // miiddleware
-app.use(express.json({limit: "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-app.use("/api/status",(req,res)=> res.send("Server is live"));
-app.use("/api/auth",userRouter);
-app.use("/api/messages",messageRouter)
+app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter)
 
 await connectDB();
-const PORT = process.env.PORT || 5000
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000
 
-server.listen(PORT,()=>console.log("Server is running on PORT :" + PORT))
+    server.listen(PORT, () => console.log("Server is running on PORT :" + PORT))
+}
+export default server;
